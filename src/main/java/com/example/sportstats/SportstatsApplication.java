8f379946095fe9bc685734e0f39930ca629d6837
Dbatr <bdanis2311@gmail.com>
2023-11-30 14:26:35 +0500

    first work

---
+++ src/main/java/com/example/sportstats/SportstatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sportstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportstatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportstatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sportstats/SportstatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sportstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportstatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
